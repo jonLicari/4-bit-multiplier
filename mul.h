1203947c5843c89b9fca71f3f6fcc94e23dbbc3e
Jonathan <jlicari@ryerson.ca>
2019-03-02 04:11:46 +0000

    mul.h edited:
    Updated comments and formatted code.

--- mul.h
+++ mul.h
@@ -6,16 +6,17 @@
 void make_mul();
 
 SC_MODULE(mul) {
+
 // PORT DECLARATION
 	sc_in<bool> clk;
-	sc_inout<sc_bv<4> > rA, rB;
-	//sc_inout<sc_bv<8> > rA, rB;
-	sc_inout<sc_bv<8> > sout; // 8 bit input registers and sum out
+	sc_inout<sc_bv<4> > rA, rB; // 4-bit registers
+	sc_inout<sc_bv<8> > sout; // 8-bit sum out
+	
 // FUNCTION DECLARATION
-	void multiply(); // args?
+	void multiply();
+	
 // CONSTRUCTOR
 	SC_CTOR(mul) {
-
 	SC_METHOD(multiply);
 	dont_initialize();
 	sensitive << clk.pos();
